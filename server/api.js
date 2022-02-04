7ef9d8628b31832470a767844d4e802a87d0e278
Bagus-myid <96364613+Bagus-myid@users.noreply.github.com>
2022-02-05 06:45:27 +0700

    Update api.js

--- server/api.js
+++ server/api.js
@@ -297,7 +297,7 @@ router.get('/kisahnabi', async(req, res) => {
 router.get('/halah', async(req, res) => {
 	var text = req.query.text
 	if (!text) return res.json({ message: 'masukan parameter text' })
-	ter = halah[1].toLowerCase()
+	ter = a[1].toLowerCase()
 	res.json(text.replace(/[aiueo]/g, ter).replace(/[AIUEO]/g, ter.toUpperCase()))
 })
 
