8a83da54cc2708f500289d3ee88a18aa10117fc6
Bagus-myid <96364613+Bagus-myid@users.noreply.github.com>
2022-02-05 12:52:32 +0700

    Update api.js

--- server/api.js
+++ server/api.js
@@ -5,8 +5,6 @@ const fetch = require('node-fetch')
 const fs = require('fs')
 const hxz = require('hxz-api')
 const zrapi = require("zrapi")
-const cheerio = require('cheerio');
-const request = require('request');
 const { getBuffer } = require('../lib/function')
 const axios = require('axios')
 const { spawn, exec } = require('child_process')
@@ -491,6 +489,17 @@ router.get('/game', async(req, res) => {
         .then(data => {
         	var result = data[Math.floor(Math.random() * data.length)];
 	res.json(result)
+	})
+})
+
+router.get('/tinyurl', async(req, res) => {
+	var url = req.query.url
+	if (!url) return res.json({ message: 'masukan parameter url' })
+	fetch(encodeURI(`https://tinyurl.com/api-create.php?url=${url}`))
+        .then(response => response.json())
+        .then(data => {
+        	var result = data
+	res.json({result})
 	})
 })
 
