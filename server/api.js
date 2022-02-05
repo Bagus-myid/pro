cc4af0dc28978fd1b41816e20b423b7300c78dd0
Bagus-myid <96364613+Bagus-myid@users.noreply.github.com>
2022-02-05 07:13:19 +0700

    Update api.js

--- server/api.js
+++ server/api.js
@@ -297,7 +297,8 @@ router.get('/kisahnabi', async(req, res) => {
 router.get('/halah', async(req, res) => {
 	var text = req.query.text
 	if (!text) return res.json({ message: 'masukan parameter text' })
-	res.json(text.replace(/[aiueo]/g, a).replace(/[AIUEO]/g, A))
+	var result =  text.replace(/[aiueo]/g, 'a').replace(/[AIUEO]/g, 'A')
+	res.json({result})
 })
 
 //RandomImageWithBuffer
